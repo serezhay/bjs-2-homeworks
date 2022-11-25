6a9f6ff5c19cc24fc3832a24cb1faf9d6325bf66
Name Serezhay <serezhay@mail.ru>
2022-11-25 23:51:59 +0500

    Task1.1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,19 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d;
+  d = b** - 4 * a * c;
+  let x;
+  let y;
+  if (d === 0) {
+    x = -b / (2*a);
+    arr.push(x)
+  }
+  if (d > 0) {
+    x = (-b +Math.sqrt(d) )/(2*a);
+    y = (-b - Math.sqrt(d) )/(2*a);
+    arr.push(x,y)
+  }
   return arr;
 }
 
