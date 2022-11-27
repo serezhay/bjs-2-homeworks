5f9332e8488e913b452231971fd1ec21f75d5bbe
Name Serezhay <serezhay@mail.ru>
2022-11-27 21:56:00 +0500

    Task1(error1.2)

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,22 +1,35 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  let d;
-  d = b** - 4 * a * c;
+  let D;
   let x;
   let y;
-  if (d === 0) {
-    x = -b / (2*a);
-    arr.push(x)
+  D = b**2 - 4*a*c;
+  if (D > 0) {
+    x = (-b + Math.sqrt(D) ) / (2*a);
+    y = (-b - Math.sqrt(D) ) / (2*a);
+    arr.push(x,y);
   }
-  if (d > 0) {
-    x = (-b +Math.sqrt(d) )/(2*a);
-    y = (-b - Math.sqrt(d) )/(2*a);
-    arr.push(x,y)
+  if (D === 0) {
+    x = -b / (2*a);
+    arr.push(x);
+  }
+  if (D < 0) {
+    arr.push();
   }
   return arr;
 }
-solveEquation(2, 5, 6)
+
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let bodyCredit;
+  let payment;
+  isNaN(percent);
+  isNaN(contribution);
+  isNaN(amount);
+  percent = (percent / 100) / countMonths;
+  bodyCredit = amount - contribution;
+  payment = bodyCredit * (percent + (percent / (((1 + percent)**countMonths) - 1)));
+  totalAmount = (payment * countMonths).toFixed(2);
+  return totalAmount;
 }
