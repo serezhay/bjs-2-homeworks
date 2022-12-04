bcfb982e2a2aa3494fe24c51a1dea04daf6482d2
Name Serezhay <serezhay@mail.ru>
2022-12-04 17:41:22 +0500

    task-3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
+    const result = arr1.every( (item, index) => item === arr2[index] && arr1.length === arr2.length)
+    return result
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(itemGender => itemGender.gender === gender).map(itemAge => itemAge.age).reduce((acc, item, index, arr) => {
+        acc+=item ;
+    if(index === arr.length - 1) {
+        return acc / arr.length;
+    } 
+    return acc;
+}, 0)
+return result
 }
